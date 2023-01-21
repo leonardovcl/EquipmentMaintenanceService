80f98a31634b7c457ef8d3ccc2045b44363a02de
Leonardo Viana <leonardovc.lima@gmail.com>
2023-01-21 20:09:03 -0300

    chore: Initialize Spring Project

---
+++ src/main/java/dev/leonardovcl/equipmentMaintenanceService/EquipmentMaintenanceServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.leonardovcl.equipmentMaintenanceService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentMaintenanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipmentMaintenanceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/leonardovcl/equipmentMaintenanceService/EquipmentMaintenanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leonardovcl.equipmentMaintenanceService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentMaintenanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
