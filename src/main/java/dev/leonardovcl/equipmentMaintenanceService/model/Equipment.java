6a54a0c0ac253808d4a915dd416e4405330ee2f2
Leonardo Viana <leonardovc.lima@gmail.com>
2023-01-22 20:09:00 -0300

    feat: Create Equipment Class closes #4

---
+++ src/main/java/dev/leonardovcl/equipmentMaintenanceService/model/Equipment.java
@@ -0,0 +1,30 @@
+package dev.leonardovcl.equipmentMaintenanceService.model;
+
+public class Equipment {
+
+	private String type;
+	
+	private String brand;
+
+	public Equipment() {
+		
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	
+}
