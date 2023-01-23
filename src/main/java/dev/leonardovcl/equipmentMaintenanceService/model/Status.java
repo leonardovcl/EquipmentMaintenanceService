e662a4abc7a121bca02759ab2628d75aeca2ef14
Leonardo Viana <leonardovc.lima@gmail.com>
2023-01-23 01:43:56 -0300

    fix(Status): Turn Status class into a entity

--- src/main/java/dev/leonardovcl/equipmentMaintenanceService/model/Status.java
+++ src/main/java/dev/leonardovcl/equipmentMaintenanceService/model/Status.java
@@ -1,11 +1,32 @@
 package dev.leonardovcl.equipmentMaintenanceService.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Status {
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@ManyToOne
+	@NotNull(message = "Must not be Null!")
+	private ServiceOrder serviceOrder;
+	
+	@ManyToOne
+	@NotNull(message = "Must not be Null!")
 	private MaintenanceEmployee employee;
 	
+	@NotNull(message = "Must not be Null!")
 	private Stage stage;
 	
+	@NotEmpty(message = "Must not be Empty!")
 	private String description;
 	
 	public enum Stage {
@@ -15,6 +36,24 @@ public class Status {
 	public Status() {
 		
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public ServiceOrder getServiceOrder() {
+		return serviceOrder;
+	}
+
+	public void setServiceOrder(ServiceOrder serviceOrder) {
+		this.serviceOrder = serviceOrder;
+	}
+
+
 
 	public MaintenanceEmployee getEmployee() {
 		return employee;
