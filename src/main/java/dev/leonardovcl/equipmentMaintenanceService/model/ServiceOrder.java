a616803cd2bb4527c88eabcc3fb7baa731ea6e44
Leonardo Viana <leonardovc.lima@gmail.com>
2023-01-23 01:46:06 -0300

    fix(ServiceOrder): Create OneToMany relationship with Status and OneToOne relationship with Equipment

--- src/main/java/dev/leonardovcl/equipmentMaintenanceService/model/ServiceOrder.java
+++ src/main/java/dev/leonardovcl/equipmentMaintenanceService/model/ServiceOrder.java
@@ -3,12 +3,16 @@ package dev.leonardovcl.equipmentMaintenanceService.model;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -22,9 +26,12 @@ public class ServiceOrder {
 	@NotNull(message = "Must not be Null!")
 	private Customer customer;
 	
+	@OneToOne(mappedBy = "serviceOrder", cascade = CascadeType.ALL)
+	@PrimaryKeyJoinColumn
 	@NotNull(message = "Must not be Null!")
 	private Equipment equipment;
 	
+	@OneToMany(mappedBy = "serviceOrder", cascade = CascadeType.ALL)
 	private List<Status> statusLog;
 	
 	@Column(name = "problem_description")
