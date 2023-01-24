b10e2ef88058010cfdc575685b7caa8d57f23417
Leonardo Viana <leonardovc.lima@gmail.com>
2023-01-24 03:54:13 -0300

    style: Correct spacing

--- src/main/java/dev/leonardovcl/equipmentMaintenanceService/model/repository/ServiceOrderRepository.java
+++ src/main/java/dev/leonardovcl/equipmentMaintenanceService/model/repository/ServiceOrderRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import dev.leonardovcl.equipmentMaintenanceService.model.ServiceOrder;
 
 public interface ServiceOrderRepository extends JpaRepository<ServiceOrder, Long> {
-
+	
 }
