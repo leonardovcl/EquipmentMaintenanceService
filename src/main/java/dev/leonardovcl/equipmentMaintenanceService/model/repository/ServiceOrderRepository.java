aa725ca9e61d5a06c72f0d2b997d89db7c0c0cd0
Leonardo Viana <leonardovc.lima@gmail.com>
2023-01-25 01:25:43 -0300

    feat(ServiceOrderRepository): Add findByCustomerId() closes #12

--- src/main/java/dev/leonardovcl/equipmentMaintenanceService/model/repository/ServiceOrderRepository.java
+++ src/main/java/dev/leonardovcl/equipmentMaintenanceService/model/repository/ServiceOrderRepository.java
@@ -1,9 +1,12 @@
 package dev.leonardovcl.equipmentMaintenanceService.model.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.leonardovcl.equipmentMaintenanceService.model.ServiceOrder;
 
 public interface ServiceOrderRepository extends JpaRepository<ServiceOrder, Long> {
 	
+	public Page<ServiceOrder> findByCustomerId(Long customerId, Pageable pageable);
 }
