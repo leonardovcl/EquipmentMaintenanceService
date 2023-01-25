ab6146ce7fd2999d4758dd644aed56b614c3d1a9
Leonardo Viana <leonardovc.lima@gmail.com>
2023-01-25 01:24:29 -0300

    feat(CustomerRepository): Add findByNameContainingIgnoreCase() closes #10

--- src/main/java/dev/leonardovcl/equipmentMaintenanceService/model/repository/CustomerRepository.java
+++ src/main/java/dev/leonardovcl/equipmentMaintenanceService/model/repository/CustomerRepository.java
@@ -1,9 +1,12 @@
 package dev.leonardovcl.equipmentMaintenanceService.model.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.leonardovcl.equipmentMaintenanceService.model.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 
+	public Page<Customer> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
